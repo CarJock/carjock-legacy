3f15e286a36953ce764e92538c4b85c18b094af7
Abdul Wahid <lbhakn0tmhtxuqewzukyvh6gzhrawz@bots.bitbucket.org>
2024-10-15 01:00:43 +0500

    fix

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -24,6 +24,7 @@
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="icon" href="favicon.ico" type="image/x-icon">
     <script>
+        var baseURL = "{{ url('/') }}";
         var siteurl = "{{ asset('/') }}";
     </script>
     <script>
